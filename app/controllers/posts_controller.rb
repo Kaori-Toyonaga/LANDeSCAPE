c863bad50232b7995e9c21b9e76ade699210319f
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-07-23 17:20:44 +0000

    add edit function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update
 
   def index
     @posts = Post.all
@@ -19,6 +19,17 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "Updated!"
+    else
+      render :edit
+    end
   end
 
   private
