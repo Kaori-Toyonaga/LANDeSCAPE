8ec20093fc97a25d396e409df797ed3c4218ae42
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-08-06 12:23:28 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :tags
   # delete "/tags/:id", to: "tags#destroy", as: :tag_delete
-  get 'sessions/new'
-  # root 'sessions/new'
+  # get 'sessions/new'
+  root 'sessions/new'
 
   resources :posts do
     collection do
