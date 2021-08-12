4ecd6283468a81ef6f4b90c23618209f88303798
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-08-12 20:15:52 +0000

    migration file recreate

--- db/migrate/20210809122258_add_columns_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToPosts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :posts, :latitude​, :float
-    add_column :posts, :longitude, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_182919) do
+ActiveRecord::Schema.define(version: 2021_08_12_192843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_08_09_182919) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.bigint "user_id"
-    t.float "latitude​"
+    t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
