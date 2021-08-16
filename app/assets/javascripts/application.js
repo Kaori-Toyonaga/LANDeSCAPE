049980ccd545f81a2600db43123e44f43e11437e
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-08-16 17:25:09 +0000

    Fixed view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-// require turbolinks
+//= require turbolinks
 //= require jquery/dist/jquery.js
-//= require bootstrap
 //= require bootstrap/dist/js/bootstrap.min
 //= require underscore
 //= require gmaps/google
