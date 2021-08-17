96dd9548648ae43bfc9ceef22776fd3b7c038a49
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-08-17 20:04:38 +0000

    route fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = 'ユーザーを登録しました'
-      redirect_to user_path(@user.id)
+      redirect_to posts_path
     else
       render :new
     end
