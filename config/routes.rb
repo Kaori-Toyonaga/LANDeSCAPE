d3a46a7a9062d612b3e04136b9f7df03a0f85cb9
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-08-18 05:42:10 +0000

    fixed route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'sessions#new'
-
   get 'relationships/create'
   get 'relationships/destroy'
+
+  root to: 'sessions#new'
 
   resources :posts do
     collection do
